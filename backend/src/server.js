611e2b93488d162c24486ba806d4f7b7ce17b021
djihane19 <kd_torchane@esi.dz>
2025-10-12 22:35:21 +0200

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import {ENV} from './config/env.js'
+
+const app = express();
+const PORT= process.env.PORT;
+app.get("/",(req,res)=>{
+    res.send("Hello World123!");
+});
+
+app.listen(ENV.PORT,()=>console.log("server started on port:",ENV.PORT));
