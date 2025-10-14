3e0be579d02a42a9dea91ae0dc4508517db677c3
djihane19 <kd_torchane@esi.dz>
2025-10-14 23:46:43 +0200

    add clerk to the front

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import {ENV} from './config/env.js'
+import {ENV} from './config/env.js';
 import { connectDB } from './config/db.js';
+import {clerkMiddleware} from "@clerk/express";
 
 const app = express();
+
+app.use(clerkMiddleware())//req.auth will be available in the request object
+
 const PORT= process.env.PORT;
 app.get("/",(req,res)=>{
+    
     res.send("Hello World123!");
 });
 
