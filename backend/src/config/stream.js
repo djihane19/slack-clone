8864e80d0a74003ad717e67ae0b0a7debe544d75
djihane19 <kd_torchane@esi.dz>
2025-10-20 22:44:36 +0200

    Edit steam.js

--- backend/src/config/stream.js
+++ backend/src/config/stream.js
@@ -1,32 +1,41 @@
 import { StreamChat } from "stream-chat";
 import { ENV } from "../config/env.js";
 
-const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET)
+const streamClient = StreamChat.getInstance(ENV.STREAM_API_KEY, ENV.STREAM_API_SECRET);
 
 export const upsertStreamUser = async (userData) => {
-    try{
-        await streamClient.upsertUser(userData);
-        console.log("Stream user upserted successfully",userData.name);
-        return userData;
-    }catch(e){
-        console.log("Error upserting Stream user:",e);
-    }
-}
-export const deleteStreamUser = async (userId)=>{
-    try{
-        await streamClient.deleteUser(userId);
-        console.log("Stream user deleted successfullly:",userId);
-    }catch(e){
-        console.error("Error deleting Stream user:",e);
-    }
+  try {
+    await streamClient.upsertUser(userData);
+    console.log("Stream user upserted successfully:", userData.name);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting Stream user:", error);
+  }
 };
 
-export const generateStreamToken =(userId)=>{
-    try{
-        const userIdString = userId.toString();
-        return streamClient.createToken(userIdString);
-    }catch(e){
-        console.log("Error creating Stream token", e);
-        return null;
-    }
-}
+export const deleteStreamUser = async (userId) => {
+  try {
+    await streamClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userId);
+  } catch (error) {
+    console.error("Error deleting Stream user:", error);
+  }
+};
+
+export const generateStreamToken = (userId) => {
+  try {
+    const userIdString = userId.toString();
+    return streamClient.createToken(userIdString);
+  } catch (error) {
+    console.log("Error generating Stream token:", error);
+    return null;
+  }
+};
+
+export const addUserToPublicChannels = async (newUserId) => {
+  const publicChannels = await streamClient.queryChannels({ discoverable: true });
+
+  for (const channel of publicChannels) {
+    await channel.addMembers([newUserId]);
+  }
+};
